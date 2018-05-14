5799cfa3578134fdc9bdda149bb8ba35f9c5583a
lasiar <cerebrum.ch@gmail.com>
2018-05-14 18:48:08 +0700

    add modal windiw

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,46 @@
+window.Event = new Vue();
+
+Vue.component('modal', {
+    template: '#modal-template',
+    props: ['succes'],
+    computed: {
+        refresh: function () {
+            return this.succes
+        }
+    }
+});
+
 Vue.component('date-form', {
     template: '#date-form-template',
     data: function () {
         return {
             date_one_start: "",
             date_one_finish: "",
+            response: "",
             loading: false
         }
     },
     props: ['who'],
     methods: {
         update: function () {
+            let self = this;
             this.loading = true;
             axios.post('/update', {
                 who: this.who,
                 date_start: this.date_one_start,
                 date_finish: this.date_one_finish
             })
-                .then(response => {
-                    if (response.data.success) {
-                        alert("Обновленно")
-                    } else {
-                        alert("Не обновленно")
-                    }
+                .then(function (response) {
+                    self.$emit('response', response.data.success);
                 })
                 .catch(function (error) {
                     alert('Ошибка! Не могу связаться с API: ' + error)
                 })
                 .finally(() => this.loading = false)
 
-        }
+        },
     },
+
     computed: {
         checkData: function () {
             if (this.date_one_start == '') {
@@ -47,6 +58,25 @@ Vue.component('date-form', {
     },
 });
 
+window.Event = new Vue();
+
 new Vue({
-    el: "#app"
+    el: "#app",
+    data: {
+        showModal: false,
+        info: "",
+        succes: false
+    },
+    methods: {
+        modal: function (succes) {
+            if (succes) {
+                this.success = "true";
+                this.info = "Обновлено"
+            } else {
+                this.success = "false";
+                this.info = "не обновлено"
+            }
+            this.showModal = true
+        }
+    }
 });
--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("assets/css/"))))
 	http.Handle("/fig/", http.StripPrefix("/fig/", http.FileServer(http.Dir("assets/fig/"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("assets/js/"))))
 	http.HandleFunc("/update", web.HandleUpdate)
