64cced4da3d1a960ea254d2a0c99086f7db4190b
lasiar <cerebrum.ch@gmail.com>
2018-05-14 14:09:27 +0700

    refactor vue js: add vue.component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,34 @@
-new Vue({
-    el: "#one",
-    data: {
-        date_one_start: "",
-        date_one_finish: "",
-        answer: "",
-        loading: false
+Vue.component('date-form', {
+    template: '#date-form-template',
+    data: function () {
+        return {
+            date_one_start: "",
+            date_one_finish: "",
+            loading: false
+        }
+    },
+    props: ['who'],
+    methods: {
+        update: function () {
+            this.loading = true;
+            axios.post('/update', {
+                who: this.who,
+                date_start: this.date_one_start,
+                date_finish: this.date_one_finish
+            })
+                .then(response => {
+                    if (response.data.success) {
+                        alert("Обновленно")
+                    } else {
+                        alert("Не обновленно")
+                    }
+                })
+                .catch(function (error) {
+                    alert('Ошибка! Не могу связаться с API: ' + error)
+                })
+                .finally(() => this.loading = false)
+
+        }
     },
     computed: {
         checkData: function () {
@@ -21,72 +45,8 @@ new Vue({
 
         }
     },
-    methods: {
-        sendPB: function () {
-            this.loading = true;
-            axios.post('/update', {
-                who: "pb",
-                date_start: this.date_one_start,
-                date_finish: this.date_one_finish
-            })
-                .then(response => {
-                    if (response.data.success) {
-                        alert("Обновленно")
-                    } else {
-                        alert("Не обновленно")
-                    }
-                })
-                .catch(function (error) {
-                    alert('Ошибка! Не могу связаться с API: ' + error)
-                })
-                .finally(() => this.loading = false)
-
-        },
-    }
 });
+
 new Vue({
-    el: "#two",
-    data: {
-        date_one_start: "",
-        date_one_finish: "",
-        answer: "",
-        loading: false
-    },
-    computed: {
-        checkData: function () {
-            if (this.date_one_start == '') {
-                return false
-            }
-            if (this.date_one_finish == '') {
-                //console.log("in if");
-                return true
-            }
-            var finish = this.date_one_finish.replace('/-/g', '');
-            var start = this.date_one_start.replace('/-/g', '');
-            return start <= finish
-
-        }
-    },
-    methods: {
-        sendAll: function () {
-            this.loading = true;
-            axios.post('/update', {
-                who: "all",
-                date_start: this.date_one_start,
-                date_finish: this.date_one_finish
-            })
-                .then(response => {
-                    if (response.data.success) {
-                        alert("Обновленно")
-                    } else {
-                        alert("Не обновленно")
-                    }
-                })
-                .catch(function (error) {
-                    alert('Ошибка! Не могу связаться с API: ' + error)
-                })
-                .finally(() => this.loading = false)
-
-        },
-    }
-})
+    el: "#app"
+});
